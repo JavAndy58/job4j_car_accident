fe910ad6031f2812198eb392b210c6f727a39925
JavAndy58 <digital.designs.russia@gmail.com>
2022-07-18 20:38:21 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/model/Officer.java
@@ -0,0 +1,5 @@
+package ru.job4j.model;
+
+public class Officer {
+    private String name;
+}
---
+++ src/main/java/ru/job4j/model/Statement.java
@@ -0,0 +1,9 @@
+package ru.job4j.model;
+
+public class Statement {
+    private String address;
+    private String carNumber;
+    private String description;
+    private byte[] photo;
+    private boolean status;
+}
