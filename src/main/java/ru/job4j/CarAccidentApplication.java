f3ca0b5510058332feaeba2dc040421745fa9ffa
JavAndy58 <digital.designs.russia@gmail.com>
2022-07-26 17:22:13 +0300

    0. Spring MVC [#6877]

--- src/main/java/ru/job4j/CarAccidentApplication.java
+++ src/main/java/ru/job4j/CarAccidentApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarAccidentApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CarAccidentApplication.class, args);
     }
