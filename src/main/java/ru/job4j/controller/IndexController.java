72aa69e8f961c57dd6709fb9ab4fa5325c2b29c8
JavAndy58 <digital.designs.russia@gmail.com>
2022-07-27 17:36:07 +0300

    2. IndexControl. Таблица и вид. [#2092]

--- src/main/java/ru/job4j/controller/IndexController.java
+++ src/main/java/ru/job4j/controller/IndexController.java
@@ -3,17 +3,24 @@ package ru.job4j.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.service.AccidentService;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class IndexController {
+    private final AccidentService accidentService;
+
+    public IndexController(AccidentService accidentService) {
+        this.accidentService = accidentService;
+    }
 
     @GetMapping("/index")
     public String index(Model model) {
         List<String> users = Arrays.asList("Petr", "Ivan", "Denis", "Olga");
         model.addAttribute("users", users);
+        model.addAttribute("accidents", accidentService.findAll());
         return "index";
     }
 }
---
+++ src/main/java/ru/job4j/service/AccidentService.java
@@ -0,0 +1,17 @@
+package ru.job4j.service;
+
+import org.springframework.stereotype.Service;
+import ru.job4j.model.Accident;
+import ru.job4j.repository.AccidentMem;
+import java.util.Collection;
+import java.util.HashMap;
+
+@Service
+public class AccidentService {
+    private final AccidentMem accidentMem = AccidentMem.instOf();
+
+    public Collection<Accident> findAll() {
+        return accidentMem.findAll();
+    }
+
+}
