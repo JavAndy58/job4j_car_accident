befa7d05775f2cb0c5a917e1c6459faffd8db947
JavAndy58 <digital.designs.russia@gmail.com>
2022-08-05 18:33:00 +0300

    4. Form с композиционным объектом [#305522]

--- src/main/java/ru/job4j/model/Accident.java
+++ src/main/java/ru/job4j/model/Accident.java
@@ -7,15 +7,17 @@ public class Accident {
     private String name;
     private String text;
     private String address;
+    private AccidentType type;
 
     public Accident() {
     }
 
-    public Accident(int id, String name, String text, String address) {
+    public Accident(int id, String name, String text, String address, AccidentType type) {
         this.id = id;
         this.name = name;
         this.text = text;
         this.address = address;
+        this.type = type;
     }
 
     public int getId() {
@@ -50,6 +52,14 @@ public class Accident {
         this.address = address;
     }
 
+    public AccidentType getType() {
+        return type;
+    }
+
+    public void setType(AccidentType type) {
+        this.type = type;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -65,15 +75,5 @@ public class Accident {
     @Override
     public int hashCode() {
         return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Accident{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", text='" + text + '\''
-                + ", address='" + address + '\''
-                +'}';
     }
 }
