4850c5ecc84ae67f86f3745cbf63305ce7dfdd73
JavAndy58 <digital.designs.russia@gmail.com>
2022-08-07 21:24:49 +0300

    5. Form с агрегационными объектами [#305523]

--- src/main/java/ru/job4j/controller/AccidentController.java
+++ src/main/java/ru/job4j/controller/AccidentController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.model.Accident;
 import ru.job4j.model.AccidentType;
+import ru.job4j.model.Rule;
 import ru.job4j.service.AccidentService;
 import ru.job4j.service.AccidentTypeService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -29,6 +31,11 @@ public class AccidentController {
     public String create(Model model) {
         Collection<AccidentType> types = accidentTypeService.findAll();
         model.addAttribute("types", types);
+        List<Rule> rules = new ArrayList<>();
+        rules.add(Rule.of(1, "Статья. 1"));
+        rules.add(Rule.of(2, "Статья. 2"));
+        rules.add(Rule.of(3, "Статья. 3"));
+        model.addAttribute("rules", rules);
         return "create";
     }
 
@@ -39,7 +46,8 @@ public class AccidentController {
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute Accident accident) {
+    public String save(@ModelAttribute Accident accident, HttpServletRequest req) {
+        String[] ids = req.getParameterValues("rIds");
         accidentService.create(accident);
         return "redirect:/index";
     }
--- src/main/java/ru/job4j/model/Accident.java
+++ src/main/java/ru/job4j/model/Accident.java
@@ -1,6 +1,7 @@
 package ru.job4j.model;
 
 import java.util.Objects;
+import java.util.Set;
 
 public class Accident {
     private int id;
@@ -8,16 +9,16 @@ public class Accident {
     private String text;
     private String address;
     private AccidentType type;
+    private Set<Rule> rules;
 
-    public Accident() {
-    }
 
-    public Accident(int id, String name, String text, String address, AccidentType type) {
+    public Accident(int id, String name, String text, String address, AccidentType type, Set<Rule> rules) {
         this.id = id;
         this.name = name;
         this.text = text;
         this.address = address;
         this.type = type;
+        this.rules = rules;
     }
 
     public int getId() {
@@ -58,6 +59,14 @@ public class Accident {
 
     public void setType(AccidentType type) {
         this.type = type;
+    }
+
+    public Set<Rule> getRules() {
+        return rules;
+    }
+
+    public void setRules(Set<Rule> rules) {
+        this.rules = rules;
     }
 
     @Override
